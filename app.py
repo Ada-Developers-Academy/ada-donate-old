6858314ddc1bdabcc8937dffa2e1c373e48ae7a2
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-05-06 12:26:36 -0700

    payment works, if you want to donate 5 dollars

--- app.py
+++ app.py
@@ -15,7 +15,26 @@ stripe.api_key = stripe_keys['secret_key']
 
 @app.route('/')
 def index():
-    return "it's working"
+    return render_template('index.html', key=stripe_keys['publishable_key'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    # Amount in cents
+    amount = 500
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        card=request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description='Flask Charge'
+    )
+
+    return render_template('charge.html', amount=amount)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

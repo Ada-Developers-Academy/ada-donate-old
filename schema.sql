e609252c56b7a58bf4e0771cde71adf7311352ec
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-05-08 17:14:40 -0700

    database has columns, verifies success

--- schema.sql
+++ schema.sql
@@ -2,5 +2,10 @@ drop table if exists donors;
 create table donors (
   id integer primary key autoincrement,
   name text,
+  email text not null,
+  success boolean not null,
+  stripe_id text not null,
+  status text not null,
+  message text not null,
   amount integer not null
 );

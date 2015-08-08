ea66983d2ce221630f83486c7bdc28a51efe1d3c
Bookis Smuin <bookis.smuin@gmail.com>
2015-08-08 12:00:35 -0700

    switch to paypal from Authorize.net

--- donation_app.py
+++ donation_app.py
@@ -1,46 +1,58 @@
 import os
 import re
+import logging
 from flask import Flask, render_template, request, g, redirect, url_for, abort, render_template, flash
 from flask.ext.sqlalchemy import SQLAlchemy
-from authorize import AuthorizeClient, CreditCard, AuthorizeError, AuthorizeInvalidError, AuthorizeResponseError
+import paypalrestsdk
 from flask_sslify import SSLify
 
 app = Flask(__name__)
-sslify = SSLify(app, permanent=True)
+app.config['DEBUG'] = os.environ.get('DEBUG', False)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQLALCHEMY_DATABASE_URI"]
-app.config["PAYMENT_SANDBOX"] = os.environ["PAYMENT_SANDBOX"] == "True"
 db = SQLAlchemy(app)
 
-class DonationForm:
-  def __init__(self, params):
-    if "customer_amount" in params:
-        self.amount = params.get("customer_amount")
-    if "customer_name" in params:
-        self.name = params["customer_name"]
-    if "customer_email" in params:
-        self.email = params["customer_email"]
-    if "card[number]" in params:
-        self.card_number       = params.get("card[number]")
-    if "card[cvc]" in params:
-        self.cvc       = params.get("card[cvc]")
-    if "card[month]" in params:
-        self.month       = params.get("card[month]")
-    if "card[year]" in params:
-        self.year       = params.get("card[year]")
+# sslify = SSLify(app, permanent=True)
 
-  def cents(self):
-      return int(float(self.amount) * 100)
+paypalrestsdk.configure({
+  "mode":          os.environ["PAYMENT_SANDBOX"], # sandbox or live
+  "client_id":     os.environ["PAYPAL_CLIENT_ID"],
+  "client_secret": os.environ["PAYPAL_SECRET"]})
+
+class DonationForm:
+    def __init__(self, params):
+        self.errors = []
+        if not params.get("customer_amount") in ["", None]:
+            self.amount = params.get("customer_amount")
+        else:
+            self.errors.append("Please enter a valid amount.")
+        if "customer_name" in params:
+            self.name = params["customer_name"]
+        if "customer_email" in params:
+            self.email = params["customer_email"]
+        if "card[type]" in params:
+            self.card_type = params.get("card[type]")
+        if "card[number]" in params:
+            self.card_number = params.get("card[number]")
+        if "card[cvc]" in params:
+            self.cvc = params.get("card[cvc]")
+        if "card[month]" in params:
+            self.month = params.get("card[month]")
+        if "card[year]" in params:
+            self.year = params.get("card[year]")
+
+    def cents(self):
+        return int(float(self.amount) * 100)
 
 class Transaction:
   def __init__(self, response):
-    self.uid           = response["transaction_id"]
-    self.type          = response["transaction_type"]
-    self.response_code = response["response_code"]
-    self.amount        = response["amount"]
-    self.message       = response["response_reason_text"]
+    self.uid           = response["transactions"][0]["related_resources"][0]["sale"]["id"]
+    self.type          = response["intent"]
+    self.response_code = response["state"]
+    self.amount        = response["transactions"][0]["amount"]["total"]
+    self.message       = response["state"]
 
   def success(self):
-    return self.response_code == "1"
+    return self.response_code == "approved"
 
 class Donor(db.Model):
     __tablename__ = 'donors'
@@ -67,15 +79,6 @@ class Donor(db.Model):
 
 # ROUTES ...
 
-# @app.before_request
-# def check_ssl():
-#     print "SSL Check"
-#     force_ssl = os.environ["SSL"] == "True"
-#     https = re.match(r'https', request.url, re.I)
-#     print request
-#     if force_ssl and not https:
-#         return redirect(url_for('index', _scheme='https', _external=True))
-
 @app.route('/')
 def index():
     form = DonationForm(request.args)
@@ -89,21 +92,41 @@ def thank_you():
 @app.route('/charge', methods=['POST'])
 def charge():
     form = DonationForm(request.form)
-    # Amount in cents
-    client = AuthorizeClient(os.environ["AUTHORIZENET_KEY"], os.environ["AUTHORIZENET_SECRET"], app.config["PAYMENT_SANDBOX"])
+    if len(form.errors) > 0:
+        return render_template('index.html', form=form, error=form.errors[0])
 
-    try:
-      cc = CreditCard(form.card_number, form.year, form.month, form.cvc)
-      card = client.card(cc, None, form.email, "Tax deductable donation to Ada Developers Academy. Tax ID: 91-1648680. 8095-3100")
-      response = card.capture(form.amount)
-    except ValueError, ex:
-      return render_template('index.html', form=form, error="Please fill in the required fields")
-    except AuthorizeInvalidError, message:
-      return render_template('index.html', form=form, error=message)
-    except AuthorizeResponseError, response:
-      return render_template('index.html', form=form, error=response.full_response["response_reason_text"])
+    payment = paypalrestsdk.Payment({
+      "intent": "sale",
+      "payer": {
+        "payer_info": {
+            "email": form.email,
+        },
+        "payment_method": "credit_card",
+        "funding_instruments": [{
+          "credit_card": {
+            "type": form.card_type,
+            "number": form.card_number,
+            "expire_month": form.month,
+            "expire_year": form.year,
+            "cvv2": form.cvc}}]},
+      "transactions": [{
+        "item_list": {
+          "items": [{
+            "name": "Tax deductable donation to Ada Developers Academy.",
+            "sku": "donation",
+            "price": form.amount,
+            "currency": "USD",
+            "quantity": 1 }]},
+        "amount": {
+          "total": form.amount,
+          "currency": "USD" },
+        "description": "Tax deductable donation to Ada Developers Academy." }]})
 
-    transaction = Transaction(response.full_response)
+
+    if not payment.create():
+        return render_template('index.html', form=form, error=payment.error["details"][0]["issue"])
+
+    transaction = Transaction(payment)
 
     donor = Donor(
         name=form.name,
